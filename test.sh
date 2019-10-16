bbb407024e08b054cb832469fb5789ea61efb441
OverLordGoldDragon <OverLordGoldDragon@yahoo.com>
2019-10-16 19:46:25 -0400

    New conditional

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pycodestyle --max-line-length=89 keras_adamw tests
 
-if [[ "$TF_VERSION" == "1.14.0" ]]; then
+if [[ "$TF_VERSION" == "1.14.1" ]]; then
     nosetests \
         --nocapture --with-coverage --cover-erase --cover-html --cover-html-dir=htmlcov --ignore-files="example.py" \
         --cover-package=keras_adamw --with-doctest tests
