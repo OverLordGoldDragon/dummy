c028c833854031d103af85a028627b6b97c1d338
OverLordGoldDragon <OverLordGoldDragon@yahoo.com>
2019-10-16 20:38:07 -0400

    Test elif

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 pycodestyle --max-line-length=89 keras_adamw tests
 
-if [[ "$TF_VERSION" == "1.14.0" ]]; then
+if [[ "$TF_VERSION" == "2.0.0" ]]; then
+    TESTPATH="tests/test_optimizers_v2.py";
+elif [[ "$TF_VERSION" == "1.14.0" ]]; then
     TESTPATH="tests/test_optimizers.py";
 fi
 
