9afca4d24d421f6cbabb885a2548acccdcb59396
OverLordGoldDragon <OverLordGoldDragon@yahoo.com>
2019-10-24 03:31:39 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@ pycodestyle --max-line-length=89 keras_adamw tests
 if [[ "$TF_KERAS" == "True" ]]; then
     TESTPATH="tests/test_optimizers_v2.py";
 else
-	TESTPATH="tests/test_optimizers.py";
+    TESTPATH="tests/test_optimizers.py";
 fi
 
 nosetests \
     --nocapture --with-coverage --cover-erase --cover-html --cover-html-dir=htmlcov \
-	--ignore-files="example.py", "tests/test_optimizers_tfpy.py" --cover-package=keras_adamw --with-doctest "$TESTPATH"
+	--ignore-files="example.py" --cover-package=keras_adamw --with-doctest "$TESTPATH"
