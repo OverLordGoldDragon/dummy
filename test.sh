edea78dfc824dfcc7a4e7b42e91707365b71c4f7
OverLordGoldDragon <OverLordGoldDragon@yahoo.com>
2019-10-25 18:58:06 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,6 +7,24 @@ else
     TESTPATH="tests/test_optimizers.py";
 fi
 
+if [[ "$TF_VERSION"=="1.14.0" ]]   && [[ "$KERAS_VERSION"=="2.2.5" ]]; then
+	IGNOREFILES = ["optimizers.py", "optimizers_225tf.py", "optimizers_tfpy.py", "utils.py", "test_optimizers_v2.py"]
+elif [[ "$TF_VERSION"=="1.14.0" ]] && [[ "$KERAS_VERSION"=="2.2.5" ]] && [[ "$TF_KERAS"=="True" ]]; then
+	IGNOREFILES = ["optimizers.py", "optimizers_225.py", "optimizers_tfpy.py", "utils.py", "test_optimizers.py"]
+elif [[ "$TF_VERSION"=="2.0.0" ]]  && [[ "$KERAS_VERSION"=="2.3.0" ]] && [[ "$TF_KERAS"=="False" ]] && [[ "TF_EAGER"=="True"]];   then
+	IGNOREFILES = ["optimizers_v2.py", "optimizers_225.py", "optimizers_225tf.py", 
+                       "optimizers_tfpy.py", "utils_225.py", "test_optimizers_v2.py"]
+elif [[ "$TF_VERSION"=="2.0.0" ]]  && [[ "$KERAS_VERSION"=="2.3.0" ]] && [[ "$TF_KERAS"=="False" ]] && [[ "TF_EAGER"=="False"]];  then
+	IGNOREFILES = ["optimizers_v2.py", "optimizers_225.py", "optimizers_225tf.py", 
+                       "optimizers_tfpy.py", "utils_225.py", "test_optimizers_v2.py"]
+elif [[ "$TF_VERSION"=="2.0.0" ]]  && [[ "$KERAS_VERSION"=="2.3.0" ]] && [[ "$TF_KERAS"=="True" ]]  && [[ "$TF_EAGER"=="True"]];  then
+	IGNOREFILES = ["optimizers.py", "optimizers_225.py", "optimizers_225tf.py", 
+                       "optimizers_tfpy.py", "utils_225.py", "test_optimizers.py"]
+elif [[ "$TF_VERSION"=="2.0.0" ]]  && [[ "$KERAS_VERSION"=="2.3.0" ]] && [[ "$TF_KERAS"=="True" ]]  && [[ "$TF_EAGER"=="False"]]; then
+	IGNOREFILES = ["optimizers.py", "optimizers_225.py", "optimizers_225tf.py", 
+                       "optimizers_tfpy.py", "utils_225.py", "test_optimizers.py"]
+IGNOREFILES = IGNOREFILES + ["example.py"]
+
 nosetests \
     --nocapture --with-coverage --cover-erase --cover-html --cover-html-dir=htmlcov \
-	--ignore-files="example.py" --cover-package=keras_adamw --with-doctest "$TESTPATH"
+	--ignore-files="$IGNOREFILES" --cover-package=keras_adamw --with-doctest "$TESTPATH"
