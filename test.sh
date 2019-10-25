a0fa77f1e5611eefaa8b7f64c7876bbf4df7db8f
OverLordGoldDragon <OverLordGoldDragon@yahoo.com>
2019-10-26 00:36:31 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,21 +8,25 @@ else
 fi
 
 if [[ "$TF_VERSION"=="1.14.0" ]] && [[ "$KERAS_VERSION"=="2.2.5" ]]; then
-	IGNOREFILES = '"optimizers.py", "optimizers_225tf.py", "utils.py", "test_optimizers_v2.py"'
+	ignorefiles = ["optimizers.py", "optimizers_225tf.py", "utils.py", "test_optimizers_v2.py"]
 elif [[ "$TF_VERSION"=="1.14.0" ]] && [[ "$KERAS_VERSION"=="2.2.5" ]] && [[ "$TF_KERAS"=="True" ]]; then
-	IGNOREFILES = '"optimizers.py", "optimizers_225.py", "utils.py", "test_optimizers.py"'
+	ignorefiles = ["optimizers.py", "optimizers_225.py", "utils.py", "test_optimizers.py"]
 elif [[ "$TF_VERSION"=="2.0.0" ]] && [[ "$KERAS_VERSION"=="2.3.0" ]] && [[ "$TF_KERAS"=="False" ]] && [[ "TF_EAGER"=="True" ]]; then
-	IGNOREFILES = '"optimizers_v2.py", "optimizers_225.py", "optimizers_225tf.py", "utils_225.py", "test_optimizers_v2.py"'
+	ignorefiles = ["optimizers_v2.py", "optimizers_225.py", "optimizers_225tf.py", "utils_225.py", "test_optimizers_v2.py"]
 elif [[ "$TF_VERSION"=="2.0.0" ]] && [[ "$KERAS_VERSION"=="2.3.0" ]] && [[ "$TF_KERAS"=="False" ]] && [[ "TF_EAGER"=="False" ]]; then
-	IGNOREFILES = '"optimizers_v2.py", "optimizers_225.py", "optimizers_225tf.py", "utils_225.py", "test_optimizers_v2.py"'
+	ignorefiles = ["optimizers_v2.py", "optimizers_225.py", "optimizers_225tf.py", "utils_225.py", "test_optimizers_v2.py"]
 elif [[ "$TF_VERSION"=="2.0.0" ]] && [[ "$KERAS_VERSION"=="2.3.0" ]] && [[ "$TF_KERAS"=="True" ]] && [[ "$TF_EAGER"=="True" ]]; then
-	IGNOREFILES = '"optimizers.py", "optimizers_225.py", "optimizers_225tf.py", "utils_225.py", "test_optimizers.py"'
+	ignorefiles = ["optimizers.py", "optimizers_225.py", "optimizers_225tf.py", "utils_225.py", "test_optimizers.py"]
 elif [[ "$TF_VERSION"=="2.0.0" ]] && [[ "$KERAS_VERSION"=="2.3.0" ]] && [[ "$TF_KERAS"=="True" ]] && [[ "$TF_EAGER"=="False" ]]; then
-	IGNOREFILES = '"optimizers.py", "optimizers_225.py", "optimizers_225tf.py", "utils_225.py", "test_optimizers.py"'
+	ignorefiles = ["optimizers.py", "optimizers_225.py", "optimizers_225tf.py", "utils_225.py", "test_optimizers.py"]
 fi
 
-IGNOREFILES = IGNOREFILES + ' "example.py", "optimizers_tfpy.py"'
+ignorefiles = ignorefiles + ["example.py", "optimizers_tfpy.py"]
+
+IGNORECOMMAND = ''
+for ignorefile in ignorefiles:
+    IGNORECOMMAND += ' --ignore-files=' + ignorefile
 
 nosetests \
     --nocapture --with-coverage --cover-erase --cover-html --cover-html-dir=htmlcov \
-	--ignore-files="$IGNOREFILES" --cover-package=keras_adamw --with-doctest "$TESTPATH"
+	"$IGNORECOMMAND" --cover-package=keras_adamw --with-doctest "$TESTPATH"
