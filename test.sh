44d3c5b62041d19159154a4d2d62fb1e9fc2acae
OverLordGoldDragon <OverLordGoldDragon@yahoo.com>
2019-10-26 19:28:58 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ fi
 ALLFILES="optimizers.py optimizers_v2.py optimizers_tfpy.py"
 ALLFILES+=" optimizers_225.py optimizers_225tf.py"
 ALLFILES+=" utils.py utils_225.py example.py"
-ALLFILES+="test_optimizers.py test_optimizers_v2.py test_optimizers_tfpy.py"
+ALLFILES+=" test_optimizers.py test_optimizers_v2.py test_optimizers_tfpy.py"
 
 
 if [[ "$TF_VERSION" == "1.14.0" ]] && [[ "$KERAS_VERSION" == "2.2.5" ]]; then
